7e1e0dbc7651f38f3f7aa8f984817e6431c36d0e
CurroRodriguez <isaac_rodriguez@live.com>
2015-10-28 10:00:17 -0400

    Added Automation for Step 3 of the Tutorial.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,27 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
-
-  beforeEach(function() {
-    browser.get('app/index.html');
+describe('PhoneCat App', function() {
+  
+  describe('Phone List View', function() {
+    
+    beforeEach(function() {
+      browser.get('app/index.html');
+    })
+    
+    it('should filter the phone list as aer types into the search box', function() {
+      
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
+      
+      expect(phoneList.count()).toBe(3);
+      
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(2);
+      
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(1);
+    });
   });
-
 });
