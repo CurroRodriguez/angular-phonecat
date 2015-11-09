fa4b44f0552ee3b6cf2864d4fb317487d3df897c
Isaac Rodriguez <isaac_rodriguez@live.com>
2015-11-09 17:44:12 -0500

    Improving automated tests.

--- test/cuke/step_definitions/phoneList.js
+++ test/cuke/step_definitions/phoneList.js
@@ -3,13 +3,13 @@
 var assert = require('assert');
 
 module.exports = function () {
-    
+
     this.Given('we are at the phone list page', function (callback) {
         this.browser.get('http://localhost:8000/app/index.html');
         callback();
     });
-    
-    
+
+
     this.When('I enter "$searchTxt" in the search box', function(searchTxt, callback) {
         var query = this.browser.element(this.By.model('query'));
         query.sendKeys(searchTxt).then(function() {
@@ -19,21 +19,21 @@ module.exports = function () {
 
 
     this.Then('all the phones are displayed', function (callback) {
-        this.browser.element.all(this.By.repeater('phone in phones')).count().then(function(count){
+        this.browser.element.all(this.By.repeater('phone in phones')).then(function(phones){
+            var count = phones.length;
             assert.equal(3, count);
             callback();
-        });        
+        });
     });
-        
+
     this.Then('$count phones are listed', function(count, callback) {
-        this.browser.element.all(this.By.repeater('phone in phones')).count().then(function(actual){
+        this.browser.element.all(this.By.repeater('phone in phones')).then(function(phones){
             var expected = parseInt(count);
-            assert.equal(expected, actual);
+            assert.equal(expected, phones.length);
             callback();
-        }); 
-        
+        });
     });
-    
-    
-    
+
+
+
 }
